15346d2d753ddd1db3d5a6e343c0a1670ad56845
Anastasiia <anastasiia@MacBook-Air-Anastasiia.local>
2025-11-25 21:24:54 +0300

    19 updates

--- Frontend/sea/src/app/app.config.ts
+++ Frontend/sea/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,9 +7,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync()
   ]
 };
+
+// используем zone.js
+//    provideZoneChangeDetection({ eventCoalescing: true }), - убрали
+
